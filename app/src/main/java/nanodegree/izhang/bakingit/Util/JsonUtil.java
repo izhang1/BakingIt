bf4f737b4fdb9b4b072424d20758d22275f7bc7e
Ivan Zhang <ivanzhangemail@gmail.com>
2017-11-02 20:43:26 -0400

    Util Classes
    Implemented util classes for the network call and the JSON call

---
+++ app/src/main/java/nanodegree/izhang/bakingit/Util/JsonUtil.java
@@ -0,0 +1,72 @@
+package nanodegree.izhang.bakingit.Util;
+
+/**
+ * Created by ivanzhang on 11/2/17.
+ */
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ivanzhang on 9/9/17.
+ * utilities
+ * - MovieJsonUtils class
+ * - This class contains methods to parse JSON objects and pull out needed information for movies.
+ * - This class contains methods to parse JSON objects and pull out needed information for trailers.
+
+ */
+
+class JsonUtil {
+
+    /**
+     *  Parses the JSON for Movie data, saves the data and returns this as a list
+     * @param movieJsonStr
+     * @return
+     * @throws JSONException
+     */
+//    public static List<Movie> getMovieListFromJson(String movieJsonStr)
+//            throws JSONException {
+//
+//        /* Results list */
+//        final String MOVIEDB_RESULT = "results";
+//
+//        /* All attributes of the movie and their information */
+//        final String MOVIE_TITLE = "title";
+//        final String MOVIE_RELEASE = "release_date";
+//        final String POSTER_PATH = "poster_path";
+//        final String MOVIE_OVERVIEW = "overview";
+//        final String VOTE_AVG = "vote_average";
+//        final String MOVIE_ID = "id";
+//
+//        /* String array to hold each day's weather String */
+//        ArrayList<Movie> movieList = new ArrayList<>();
+//
+//        JSONObject movieJson = new JSONObject(movieJsonStr);
+//
+//        JSONArray movieArray = movieJson.getJSONArray(MOVIEDB_RESULT);
+//
+//        /* Looping through the array and saving the movie values into a list of movies passed back to the calling method. */
+//        for (int i = 0; i < movieArray.length(); i++) {
+//            JSONObject tempObj = (JSONObject) movieArray.get(i);
+//            //Log.v("MovieJsonUtils", tempObj.toString());
+//            Movie movie = new Movie(tempObj.getString(MOVIE_TITLE),
+//                    tempObj.getString(MOVIE_RELEASE),
+//                    tempObj.getString(POSTER_PATH),
+//                    tempObj.getString(MOVIE_OVERVIEW),
+//                    tempObj.getInt(VOTE_AVG),
+//                    tempObj.getString(MOVIE_ID));
+//            movieList.add(movie);
+//        }
+//
+//        return movieList;
+//    }
+//
+
+
+}
---
+++ app/src/main/java/nanodegree/izhang/bakingit/Util/NetworkUtils.java
@@ -0,0 +1,81 @@
+package nanodegree.izhang.bakingit.Util;
+
+/**
+ * Created by ivanzhang on 11/2/17.
+ */
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * These utilities will be used to communicate with the weather servers.
+ */
+public final class NetworkUtils {
+
+    private static final String RECIPE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+
+    /*
+     * NOTE: These values only effect responses from OpenWeatherMap, NOT from the fake weather
+     * server. They are simply here to allow us to teach you how to build a URL if you were to use
+     * a real API.If you want to connect your app to OpenWeatherMap's API, feel free to! However,
+     * we are not going to show you how to do so in this course.
+     */
+
+    public static final int REVIEWS = 400;
+
+
+    /**
+     * Builds the URL based on the passed in sort the user wants.
+     *
+     * @param passedCategory The sort that will be queried for.
+     * @return The URL to use to query the service.
+     */
+    public static URL buildUrl(int passedCategory) {
+
+        Uri builtUri;
+
+        builtUri = Uri.parse(RECIPE_URL);
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return url;
+    }
+
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
