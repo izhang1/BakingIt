4b6be024ab09da42589282555d6312dc8212e422
Ivan Zhang <ivanzhangemail@gmail.com>
2017-12-17 13:58:49 -0500

    Ingredient Adapter and Item Changes

---
+++ app/src/main/java/nanodegree/izhang/bakingit/IngredientAdapter.java
@@ -0,0 +1,76 @@
+package nanodegree.izhang.bakingit;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by ivanzhang on 12/17/17.
+ */
+
+public class IngredientAdapter extends RecyclerView.Adapter<IngredientAdapter.IngredientViewHolder> {
+
+    private ArrayList<String> mData;
+    private Context context;
+
+    public IngredientAdapter(ArrayList<String> data) {
+        this.mData = data;
+    }
+
+
+    public void setData(ArrayList<String> data){
+        this.mData = data;
+    }
+
+    @Override
+    public IngredientAdapter.IngredientViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.ingredient_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new IngredientAdapter.IngredientViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(IngredientViewHolder holder, int position) {
+        holder.textView.setText(mData.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(mData == null) return 0;
+        else return mData.size();
+    }
+
+    /**
+     * RecipeViewHolder class - inner
+     * - Initializes the view and sets the value as well as implements the onclick functions
+     *
+     */
+    public class IngredientViewHolder extends RecyclerView.ViewHolder{
+
+        @BindView(R.id.tv_recipe_name)
+        TextView textView;
+
+        public IngredientViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+    }
+
+
+}
+
