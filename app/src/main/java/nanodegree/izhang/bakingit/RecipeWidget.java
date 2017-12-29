ce3af0c49f0392be41e3fb74fb2f7499d6263183
Ivan Zhang <ivanzhangemail@gmail.com>
2017-12-29 09:59:23 -0500

    RecipeWidget Provider
    - Removed duplicate code in the onUpdate and updateAppWidget methods.

--- app/src/main/java/nanodegree/izhang/bakingit/RecipeWidget.java
+++ app/src/main/java/nanodegree/izhang/bakingit/RecipeWidget.java
@@ -48,28 +48,8 @@ public class RecipeWidget extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            // Set up the intent that starts the StackViewService, which will
-            // provide the views for this collection.
-            Intent intent = new Intent(context, RecipeService.class);
-            // Add the app widget ID to the intent extras.
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
-            // Instantiate the RemoteViews object for the app widget layout.
-            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.recipe_widget);
-            // Set up the RemoteViews object to use a RemoteViews adapter.
-            // This adapter connects
-            // to a RemoteViewsService  through the specified intent.
-            // This is how you populate the data.
-            rv.setRemoteAdapter(R.id.list_recipes, intent);
 
-            // The empty view is displayed when the collection has no items.
-            // It should be in the same layout used to instantiate the RemoteViews
-            // object above.
-            //rv.setEmptyView(R.id.stack_view, R.id.empty_view);
-
-            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetId, R.id.list_recipes);
-
-            appWidgetManager.updateAppWidget(appWidgetId, rv);
+            RecipeWidget.updateAppWidget(context, appWidgetManager, appWidgetId);
         }
 
         super.onUpdate(context, appWidgetManager, appWidgetIds);
